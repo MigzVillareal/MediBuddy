7351b389ba216915485cdec8b0c62f133236ab59
Miguel Villareal <migzvillareal27@gmail.com>
2026-05-17 01:06:03 +0800

    models.py +user +medlookup +medsupply

--- backend/models/models.py
+++ backend/models/models.py
@@ -10,29 +10,54 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # def load_user(id):
 #     return db.session.get(User, int(id))
 
-class Drug_Lookup(db.Model):
-    __tablename__ = "drug_lookup"
+# USER
 
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(36), unique=True, index=True, nullable=False)
+    password_hash = db.Column(db.String(256))
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    first_name = db.Column(db.String(64), nullable=True)
+    last_name = db.Column(db.String(64), nullable=True)
+
+
+# MED_LOOKUP
+
+class Med_Lookup(db.Model):
+    __tablename__ = "Med_Lookup"
+
+    lookup_id: so.Mapped[int] = so.mapped_column(primary_key=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     brand_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
     generic_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
-    dosage_form: so.Mapped[Optional[str]] = so.mapped_column(sa.String(50))
+    dosage_strength: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
+    dosage_form: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
 
-class Drug_Stock(db.Model):
-    __tablename__ = "drug_stock"
 
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+# MED_SUPPLY
+
+class Med_Supply(db.Model):
+    __tablename__ = "Med_Supply"
+
+    supply_id: so.Mapped[int] = so.mapped_column(primary_key=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-
-    brand_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
-    generic_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
-    dosage_form: so.Mapped[Optional[str]] = so.mapped_column(sa.String(50))
 
     quantity: so.Mapped[int] = so.mapped_column(sa.Integer)
+    # to put: [intakes_left(supply_left/ dosage_strength)]
+    expiration_date: so.Mapped[Optional[str]] = so.mapped_column(sa.String(120))
+
+
+# MED_LOOKUP
 
 class CircleMember(db.Model):
     __tablename__ = "circle_members"

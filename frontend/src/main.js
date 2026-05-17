a84a81f0739feb2c550675c2a0b76d6b41903a7e
Miguel Villareal <migzvillareal27@gmail.com>
2026-05-17 20:03:56 +0800

    fix OneSignal service worker route

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
-import OneSignalVuePlugin from 'onesignal-vue3'
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 
 const app = createApp(App)
 

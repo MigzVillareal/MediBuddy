e4ac5674aad6d80237115905b17033dd86a0f74c
Miguel Villareal <migzvillareal27@gmail.com>
2026-05-17 12:05:44 +0800

    fix vercel deployment structure

--- api/index.py
+++ api/index.py
@@ -3,7 +3,9 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'backend'))
 
-from app import app
+from app import create_app
+
+flask_app = create_app()
 
 def handler(environ, start_response):
-    return app(environ, start_response)
+    return flask_app(environ, start_response)

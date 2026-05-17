6f006a83bcaa46df9ad6ef3ac2c2b90f26c90144
Miguel Villareal <migzvillareal27@gmail.com>
2026-05-17 12:20:06 +0800

    fix register_routes and login.init_app

--- backend/app.py
+++ backend/app.py
@@ -2,31 +2,29 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 from config import Config
-from extensions import db
 from extensions import db, login
 from routes import register_routes
 
 def create_app():
-    # initialize flask object
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # database initialization
     db.init_app(app)
+    login.init_app(app)
 
-    # CORS Config
     CORS(
         app,
-        supports_credentials = True,
+        supports_credentials=True,
         origins=app.config["CORS_ORIGINS"],
         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allow_headers=["Content-Type", "Authorization"],
     )
+
+    register_routes(app)
 
     return app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)

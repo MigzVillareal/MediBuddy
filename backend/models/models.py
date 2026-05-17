a818649d8e0e84b51cefe5ca6f8d8f7515514ae1
Miguel Villareal <migzvillareal27@gmail.com>
2026-05-17 12:50:55 +0800

    add get_id to User model

--- backend/models/models.py
+++ backend/models/models.py
@@ -18,14 +18,17 @@ class User(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(36), unique=True, index=True, nullable=False)
     password_hash = db.Column(db.String(256))
+    first_name = db.Column(db.String(64), nullable=True)
+    last_name = db.Column(db.String(64), nullable=True)
+
+    def get_id(self):
+        return str(self.user_id)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
-    first_name = db.Column(db.String(64), nullable=True)
-    last_name = db.Column(db.String(64), nullable=True)
 
 
 # PRESCRIPTION

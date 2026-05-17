408d134fdb9856476c113ac2f5f61a13c429c922
Miguel Villareal <migzvillareal27@gmail.com>
2026-05-17 18:31:30 +0800

    add OneSignal service worker

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,12 @@ import App from './App.vue'
 import router from './router'
 import  './assets/main.css'
 
+window.OneSignalDeferred = window.OneSignalDeferred || []
+OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+        appId: "YOUR_APP_ID",
+    })
+})
+
 createApp(App).use(router).mount("#app");
 

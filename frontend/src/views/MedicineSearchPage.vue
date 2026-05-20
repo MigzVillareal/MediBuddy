ad980018d234fe1424c861be5254f466a5909330
cleofejianjoshua <cleofe.jianjoshua@gmail.com>
2026-05-20 19:54:27 +0800

    Update MedicineSearchPage.vue

--- frontend/src/views/MedicineSearchPage.vue
+++ frontend/src/views/MedicineSearchPage.vue
@@ -38,9 +38,9 @@
           <div>
             <p class="result-card__brand">{{ med.brand_name }}</p>
             <p class="result-card__name">{{ med.generic_name }}</p>
-            <span class="med-card__form">{{ truncateAtBracket(med.dosage_form) }}</span>
-            <span class="med-card__form">{{ truncateAtBracket(med.category) }}</span>
-            <span class="result-card__form">{{ med.category }}</span>
+            <span class="result-card__form">{{ med.dosage_strength }}</span>
+            <span class="result-card__form">{{ truncateAtBracket(med.dosage_form) }}</span>
+            <span class="result-card__form">{{ truncateAtBracket(med.category) }}</span>
           </div>
           <span class="result-card__arrow">›</span>
         </div>
